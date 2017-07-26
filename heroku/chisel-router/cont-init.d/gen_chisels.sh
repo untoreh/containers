cfc728291d6ba6390574bba9b809640bbb4b7c16
untoreh <bassobassista@gmail.com>
2017-07-26 12:47:39 +0200

    chisel-router

---
+++ heroku/chisel-router/cont-init.d/gen_chisels.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv sh
+
+c=1
+for EP in $ENDPOINTS; do
+    srv_dir=/etc/services.d/chisel-client-${c}
+    mkdir -p $srv_dir
+    cat <<EOF > ${srv_dir}/run
+#!/bin/sh
+
+exec chisel client ${EP} 127.0.0.${c}:${EP_PORT}
+EOF
+    chmod +x ${srv_dir}/run
+    c=$((c+1))
+done
