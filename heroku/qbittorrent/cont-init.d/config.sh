48c6794d0009c66b96e9582bce64c6a43bb504a4
untoreh <bassobassista@gmail.com>
2017-08-17 08:20:24 +0200

    .

--- heroku/qbittorrent/cont-init.d/config.sh
+++ heroku/qbittorrent/cont-init.d/config.sh
@@ -3,3 +3,7 @@
 if [ ! -z "$QBTR_CONFIG" ]; then
 	echo "$QBTR_CONFIG" | base64 -d > /config/qBittorrent.conf
 fi
+
+if [ ! -z "$ST_CONFIG" ]; then
+	echo "$ST_CONFIG" | base64 -d > /stconfig/config.xml
+fi
